2f79a9b66ee5a5867fd887c3030f6f0fa1b60891
Nguyen Viet Cuong <cuong-nv@trente.asia>
2019-06-14 17:30:09 +0700

    remove room

--- app/src/main/java/com/example/trente/myapplication/Tictactoe/RoomDetailFragment.java
+++ app/src/main/java/com/example/trente/myapplication/Tictactoe/RoomDetailFragment.java
@@ -18,6 +18,7 @@ import com.example.trente.myapplication.App;
 import com.example.trente.myapplication.R;
 import com.example.trente.myapplication.Tictactoe.Model.RoomModel;
 import com.example.trente.myapplication.base.BaseGetRequest;
+import com.example.trente.myapplication.base.BasePostRequest;
 import com.example.trente.myapplication.base.OnResponseListener;
 import com.example.trente.myapplication.model.Utils;
 import com.google.gson.JsonObject;
@@ -46,6 +47,7 @@ public class RoomDetailFragment extends Fragment {
     public TextView player2Name;
     public Button btnClear;
     public Button btnStart;
+    public Button btnCancel;
 
     public String response = "";
     public Timer timer;
@@ -65,6 +67,18 @@ public class RoomDetailFragment extends Fragment {
         player2Name = (TextView)getView().findViewById(R.id.txt_name2);
         btnClear = (Button)getView().findViewById(R.id.btn_clear);
         btnStart = (Button)getView().findViewById(R.id.btn_start);
+        btnCancel = (Button)getView().findViewById(R.id.btn_cancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isAdmin){
+                    deleteRoom(room.roomid);
+                }else {
+                    updateRoomJoiner(room.roomid, null, null);
+                }
+            }
+        });
 
         if(isAdmin){
             btnStart.setVisibility(View.VISIBLE);
@@ -119,9 +133,13 @@ public class RoomDetailFragment extends Fragment {
             this.response = response;
             try {
                 JSONObject object = new JSONObject(response);
-                RoomModel room = LoganSquare.parse(object.optString("room"),RoomModel.class);
-                this.room = room;
-                showRoom(room);
+                if("1".equals(object.optString("returncode"))) {
+                    RoomModel room = LoganSquare.parse(object.optString("room"), RoomModel.class);
+                    this.room = room;
+                    showRoom(room);
+                }else {
+                    getFragmentManager().popBackStack();
+                }
             } catch (JSONException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -144,6 +162,85 @@ public class RoomDetailFragment extends Fragment {
             }
         }
 
+    }
+
+    public void updateRoomJoiner(String roomId, String joinerId, String joinerName){
+        if (!Utils.isNetworkAvailable(getActivity())){
+            return;
+        } else {
+            OnResponseListener<JsonObject> listener = new OnResponseListener<JsonObject>(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                    super.onErrorResponse(error);
+                }
+
+                @Override
+                public void onResponse(JsonObject response) {
+
+                    super.onResponse(response);
+                    try {
+                        JSONObject object = new JSONObject(response.toString());
+                        if("1".equals(object.optString("returncode"))){
+                            getFragmentManager().popBackStack();
+                        }else {
+                            ((TictacActivity)getActivity()).showMessage("error!");
+                        }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            BasePostRequest request = new BasePostRequest( "http://192.168.1.125:8888/apiroom.php/update_room_joiner",
+                    new TypeToken<JsonObject>(){}.getType(),listener);
+
+            request.setParam("joinername", joinerName);
+            request.setParam("joinerid", joinerName);
+            request.setParam("roomid", roomId);
+            App.addRequest(request);
+
+        }
+    }
+
+    public void deleteRoom(String roomId){
+        if (!Utils.isNetworkAvailable(getActivity())){
+            return;
+        } else {
+            OnResponseListener<JsonObject> listener = new OnResponseListener<JsonObject>(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    super.onErrorResponse(error);
+                }
+
+                @Override
+                public void onResponse(JsonObject response) {
+
+                    super.onResponse(response);
+                    try {
+                        JSONObject object = new JSONObject(response.toString());
+                        if("1".equals(object.optString("returncode"))){
+                            getFragmentManager().popBackStack();
+                        }else {
+                            ((TictacActivity)getActivity()).showMessage("error!");
+                        }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            BasePostRequest request = new BasePostRequest( "http://192.168.1.125:8888/apiroom.php/delete_room",
+                    new TypeToken<JsonObject>(){}.getType(),listener);
+
+            request.setParam("roomid", roomId);
+            App.addRequest(request);
+
+        }
     }
 
 
